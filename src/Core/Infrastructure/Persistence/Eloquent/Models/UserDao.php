74537f5db85e0f6a0f95c54d76208babb95b9624
danimc97 <dani1997medina@gmail.com>
2022-08-23 22:06:09 +0200

    Configure JWT

--- src/Core/Infrastructure/Persistence/Eloquent/Models/UserDao.php
+++ src/Core/Infrastructure/Persistence/Eloquent/Models/UserDao.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class UserDao extends Authenticatable
+class UserDao extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -52,4 +53,24 @@ class UserDao extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
