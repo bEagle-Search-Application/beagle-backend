54233e6984f1bb0834a58e8fc1f6aa4021f354d8
danimc97 <dani1997medina@gmail.com>
2022-10-10 19:30:40 +0200

    CU-3ggw4px Fix UserNotFound byId method

--- src/Core/Domain/User/Errors/UserNotFound.php
+++ src/Core/Domain/User/Errors/UserNotFound.php
@@ -8,18 +8,12 @@ use Beagle\Core\Domain\User\ValueObjects\UserId;
 final class UserNotFound extends \Exception
 {
     private const INVALID_CREDENTIALS = "Las credenciales de %s son incorrectas";
-    private const INVALID_TOKEN = "El token es inválido";
     private const INVALID_EMAIL = "El usuario con email %s no existe";
     private const INVALID_ID = "El usuario con id %s no existe";
 
     public static function byCredentials(UserEmail $userEmail): self
     {
         return new self(\sprintf(self::INVALID_CREDENTIALS, $userEmail->value()));
-    }
-
-    public static function byToken(): self
-    {
-        return new self(self::INVALID_TOKEN);
     }
 
     public static function byEmail(UserEmail $email):self
@@ -29,6 +23,6 @@ final class UserNotFound extends \Exception
 
     public static function byId(UserId $userId):self
     {
-        return new self(self::INVALID_ID);
+        return new self(\sprintf(self::INVALID_ID, $userId->value()));
     }
 }
