4a1ea8b584f844d290a50732c30098665395af2a
danimc97 <dani1997medina@gmail.com>
2022-10-12 11:29:50 +0200

    CU-3khzybe Delete validations on UserPassword

--- src/Core/Domain/User/ValueObjects/UserPassword.php
+++ src/Core/Domain/User/ValueObjects/UserPassword.php
@@ -2,19 +2,10 @@
 
 namespace Beagle\Core\Domain\User\ValueObjects;
 
-use Beagle\Shared\Domain\Errors\InvalidPassword;
-
 final class UserPassword
 {
-    private const MIN_LENGTH = 8;
-
-    /** @throws InvalidPassword */
     private function __construct(private string $value)
     {
-        if (\strlen($this->value) < self::MIN_LENGTH)
-        {
-            throw InvalidPassword::byLength(self::MIN_LENGTH);
-        }
     }
 
     public function value():string
@@ -22,7 +13,6 @@ final class UserPassword
         return $this->value;
     }
 
-    /** @throws InvalidPassword */
     public static function fromString(string $password):self
     {
         return new self($password);
--- tests/MotherObjects/User/ValueObjects/UserPasswordMotherObject.php
+++ tests/MotherObjects/User/ValueObjects/UserPasswordMotherObject.php
@@ -3,18 +3,15 @@
 namespace Tests\MotherObjects\User\ValueObjects;
 
 use Beagle\Core\Domain\User\ValueObjects\UserPassword;
-use Beagle\Shared\Domain\Errors\InvalidPassword;
 use Faker\Factory;
 
 final class UserPasswordMotherObject
 {
-    /** @throws InvalidPassword */
     public static function create(?string $password = null):UserPassword
     {
         return UserPassword::fromString($password ?? self::createNumberAsString());
     }
 
-    /** @throws InvalidPassword */
     public static function createWithHash(?string $password = null):UserPassword
     {
         return UserPassword::fromString($password ?? \md5(self::createNumberAsString()));
