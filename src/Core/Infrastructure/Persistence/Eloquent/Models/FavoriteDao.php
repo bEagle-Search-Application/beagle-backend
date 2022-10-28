4259f9251b8f4b593f80674fb99ae6a920bf7389
danimc97 <dani1997medina@gmail.com>
2022-10-28 16:37:48 +0200

    CU-3qa1xtk Add field constants in remaining DAOs

--- src/Core/Infrastructure/Persistence/Eloquent/Models/FavoriteDao.php
+++ src/Core/Infrastructure/Persistence/Eloquent/Models/FavoriteDao.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 final class FavoriteDao extends Model
 {
+    public const ID = 'id';
+    public const USER_ID = 'user_id';
+    public const AD_ID = 'ad_id';
+
     protected $table = 'favorites';
     protected $keyType = 'string';
     protected $fillable = [
+        'id',
         'user_id',
         'ad_id',
     ];
